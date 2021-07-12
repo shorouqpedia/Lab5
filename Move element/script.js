8fc34b9948366aec9bbb68f585ebaa5099a3a5f8
Shorouq Fayez <shorouq96@yahoo.com>
2021-07-12 21:36:47 +0200

    delete

--- Move element/script.js
+++
@@ -1,8 +0,0 @@
-img=document.getElementById('hi');
-
-document.onmousemove = function(e) { 
-    var x = e.clientX; 
-    var y = e.clientY; 
-    img.style.marginLeft = x+"px";
-    img.style.marginTop = y+"px";
-}
